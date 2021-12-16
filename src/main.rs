a4e1becd093e8a6f89b42612f75a25628accf34a
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-16 18:07:24 -0300

    feat: implemented image combine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,127 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+use args::Args;
+use image::{ io::Reader, DynamicImage, ImageFormat, imageops::FilterType::Triangle, GenericImageView };
+use std::{ io::BufReader, fs::File };
+
+#[derive(Debug)]
+enum ImageDataErrors {
+    DifferentFormat,
+    BufferTooSmall,
+}
+
+struct FloatingImage {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    name: String,
+}
+
+impl FloatingImage {
+    fn new(width: u32, height: u32, name: String) -> Self {
+        let buffer_capacity = height * width * 4;
+        let buffer = Vec::with_capacity(buffer_capacity.try_into().unwrap());
+        FloatingImage {
+            width,
+            height,
+            data: buffer,
+            name,
+        }
+    }
+    fn set_data(&mut self, data: Vec<u8>) -> Result<(), ImageDataErrors> {
+        if data.len() != self.data.capacity() {
+            return Err(ImageDataErrors::BufferTooSmall);
+        }
+        self.data = data;
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), ImageDataErrors> {
+    let args: Args = Args::new();
+
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+
+    if image_format_1 != image_format_2 {
+        println!("Images are not of the same format");
+        return Err(ImageDataErrors::DifferentFormat); 
+    }
+
+    let (image_1, image_2) = standardize_size(image_1, image_2);
+    let mut output_image = FloatingImage::new(image_1.width(), image_1.height(), args.output);
+
+    let combined_data = combine_images(image_1, image_2);
+    output_image.set_data(combined_data)?;
+
+    image::save_buffer_with_format(output_image.name, &output_image.data, output_image.width, output_image.height, image::ColorType::Rgba8, image_format_1).unwrap();
+
+    Ok(())
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
+}
+
+fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pixel_count_1 = dim_1.0 * dim_1.1;
+    let pixel_count_2 = dim_2.0 * dim_2.1;
+
+    if pixel_count_1 < pixel_count_2 {
+        return dim_1;
+    } else {
+        return dim_2;
+    }
+}
+
+fn standardize_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, height) = get_smallest_dimension(image_1.dimensions(), image_2.dimensions());
+    println!("width: {} height: {}", width, height);
+
+    if image_2.dimensions() != (width, height) {
+        let image_2_resized = image_2.resize_exact(width, height, Triangle);
+        return (image_1, image_2_resized);
+    } else {
+        let image_1_resized = image_1.resize_exact(width, height, Triangle);
+        return (image_1_resized, image_2);
+    }
+}
+
+fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
+    let vec_1 = image_1.to_rgba8().into_vec();
+    let vec_2 = image_2.to_rgba8().into_vec();
+
+    alternate_pixels(vec_1, vec_2)
+}
+
+fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+    let mut combined_data = vec![0u8; vec_1.len()];
+
+    let mut i = 0;
+    while i < vec_1.len() {
+        
+        if i % 8 == 0 {
+            combined_data.splice(i..=i + 3, set_rgba(&vec_1, i, i + 3));
+        } else {
+            combined_data.splice(i..=i + 3, set_rgba(&vec_2, i, i + 3));
+        }
+
+        i += 4;
+    }
+
+    combined_data
+}
+
+fn set_rgba(vec: &Vec<u8>, start: usize, end: usize) -> Vec<u8> {
+    let mut rgba = Vec::new();
+    for i in start..=end {
+        let val = match vec.get(i) {
+            Some(val) => *val,
+            None => panic!("Could not get value from vec"),
+        };
+        rgba.push(val);
+    }
+    rgba
 }
